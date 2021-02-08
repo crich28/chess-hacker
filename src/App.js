964a10884ba03b07ab4c99f744e26e154a7a82b2
Dom <59603300+TokyoDom@users.noreply.github.com>
2021-02-08 17:15:01 +0900

    connect frontend with socketioclient

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import { useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const URL = "http://localhost:5000";
 
 function App() {
+
+  useEffect(() => {
+    const socket = socketIOClient(URL);
+  });
+
   return (
     <div className="App">
       Hello World
