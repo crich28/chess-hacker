b7f5e46cae1e665f42534a1ab200f1646a096e9d
Dom <59603300+TokyoDom@users.noreply.github.com>
2021-02-08 17:13:02 +0900

    basic socketio config

--- server/index.js
+++ server/index.js
@@ -1,16 +1,7 @@
 require("dotenv").config();
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-app.use(express.static(path.resolve(__dirname, "..", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
-});
+const server = require("./server");
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("App listening on port " + port);
 });
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.get("*", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+});
+
+module.exports = server;
